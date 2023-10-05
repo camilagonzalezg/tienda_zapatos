79899b1b031339a44da8902ee310dc3265f43fb7
camilagonzalezg <camilag@gmail.com>
2023-10-05 17:05:47 -0300

    Se agregan imagenes, además de la página producto individual, la cual se perfila según el id de cada producto. Se avanza links y conectividad

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('productos/', views.productos, name='productos'),
-    path('producto/', views.producto, name='producto'),
     path('formulario/', views.formulario, name='formulario'),
+    path('producto/<int:producto_id>/', views.producto, name='producto'),
 ]
